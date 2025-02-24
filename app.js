2c83252a9ec8bf3cb9703bf4463893a84d7865ea
Dmoore8122 <173196877+Dmoore8122@users.noreply.github.com>
2025-02-24 12:42:30 -0500

    Added a new container to be added and removed for new demision set by user.

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const container = document.querySelector(".container");
+const resetButton = document.querySelector('button')
 
-const sizeOfGrid = 16;
 
 const createGrid = (numOfGrids) => {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
+
     for (let i = 0; i < numOfGrids; i++) {
         const row = document.createElement('div');
         row.classList.add('grid-row');
 
         for (let j = 0; j < numOfGrids; j++) {
+            const widthAndHeight = 960 / numOfGrids;
             const gridBox = document.createElement('div');
             gridBox.classList.add('grid-box');
+            gridBox.style.width = `${widthAndHeight}px`;
+            gridBox.style.height = `${widthAndHeight}px`;
 
             gridBox.addEventListener('mouseenter', () => {
                 gridBox.style.backgroundColor = 'black';
@@ -18,8 +24,27 @@ const createGrid = (numOfGrids) => {
             row.appendChild(gridBox);
         }
 
-        container.appendChild(row);
+        wrapper.appendChild(row);
     }
+
+    container.appendChild(wrapper);
 }
 
-createGrid(sizeOfGrid); 
+
+resetButton.addEventListener('click', () => {
+    let userSize = Number(prompt('What dimensions do you want to sketch on?'));
+
+    while (userSize > 100) {
+        userSize = Number(prompt('Smaller please, less than 100?!'));
+    }
+
+    const wrapper = document.querySelector('.wrapper');
+    
+    if (!wrapper) {
+        createGrid(userSize);
+    } else {
+        wrapper.remove();
+        createGrid(userSize);
+    }
+})
+
